7383c0dbb92dc699f3dbc4916120b6bc3e84ff80
Prosenjit Barman <prosenjitbarmann@gmail.com>
2022-06-23 13:42:58 +0600

    Simple Portfolio

---
+++ app.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'pug'); //pug is used as template engine.
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, './public')));
+
+let userCount = 0;
+
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        title: "Portfolio | Prosenjit",
+        name: "PROSENJIT BARMAN",
+        visitor: userCount++
+    });
+})
+
+
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`The Server is running on port ${port}`)
+});
